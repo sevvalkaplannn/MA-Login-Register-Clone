a4f7befa5da239cca2e824e4566623e5e724e8f9
Şevval Kaplan <sevval.kaplan@mobileaction.co>
2023-07-27 11:18:56 +0300

    feat(OFB-204):The main template is done

--- src/components/MaButton.vue
+++ src/components/MaButton.vue
@@ -6,7 +6,8 @@ const props = defineProps({
   disabled: Boolean,
   intent: {
     type: String,
-    validator: (val) => ["primary","ma-secondary-just-logo", "secondary", "header-primary"].includes(val),
+    validator: (val) =>
+      ["primary", "ma-secondary-just-logo", "secondary", "header-primary"].includes(val),
   },
 });
 
@@ -14,14 +15,11 @@ const buttonClass = computed(() => {
   const baseClass = "ma-button-default";
   if (props.intent === "primary") {
     return `${baseClass} ma-primary`;
-  }
-  else if (props.intent === "ma-secondary-just-logo") {
+  } else if (props.intent === "ma-secondary-just-logo") {
     return `${baseClass} ma-secondary-just-logo`;
-  }
-  else if (props.intent === "header-primary") {
+  } else if (props.intent === "header-primary") {
     return `${baseClass} header-primary`;
-  }
-   else {
+  } else {
     return `${baseClass} ma-secondary`;
   }
 });
@@ -41,21 +39,22 @@ const buttonClass = computed(() => {
   @apply bg-PrimaryDark max-[1024px]:w-full text-white font-medium w-[360px] hover:bg-PrimaryHover border-none;
 }
 
-.ma-secondary-just-logo{
-  @apply bg-white h-[43.6px] max-[1024px]:w-full w-[112px] text-black border-BorderGray border border-solid font-semibold hover:bg-SecondaryHover;;
+.ma-secondary-just-logo {
+  @apply bg-white h-[43.6px] max-[1024px]:w-full w-[112px] text-black border-BorderGray border border-solid font-semibold hover:bg-SecondaryHover;
 }
 
-.ma-secondary-just-logo:active{
+.ma-secondary-just-logo:active {
   @apply shadow-[0px_0px_0px_4px_#f2f4f7,0px_1px_2px_0px_rgba(16,24,40,0.05)];
 }
 
-.header-primary{
+.header-primary {
   @apply h-[38.8px] w-[180px] no-underline font-bold px-2 py-1 border-[#2125] cursor-pointer text-[1.2rem] font-euclid leading-normal rounded-[0.2rem]  bg-[#f8f9fa];
-  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   border: 1px solid;
 }
 
-.header-primary:hover{
+.header-primary:hover {
   color: #212529;
   background-color: #e2e6ea;
 }
--- src/views/MaHome.vue
+++ src/views/MaHome.vue
@@ -2,25 +2,29 @@
   <div class="ma-page">
     <div class="ma-home-banner">
       <div class="ma-infos">
-        <a href="https://www.mobileaction.co/contact-us/" class="">Contact</a>
-        <hr class="ma-divider-span" />
-        <RouterLink to="/login" class="font-bold"> Login </RouterLink>
-        <hr class="ma-divider-span" />
-        <a
-          href="https://searchads.com/?__hstc=212712200.c711629e081c1750ce8d4392ba12b1c6.1683644286291.1689761584422.1690370044196.8&amp;__hssc=212712200.1.1690370044196&amp;__hsfp=147110142"
-          target="_blank"
-          rel="noopener"
-          class="pr-1"
-        >
-          Go to <b>SearchAds</b></a
-        >
+        <div class="ma-infos-wrapper">
+          <a href="https://www.mobileaction.co/contact-us/" class="">Contact</a>
+          <hr class="ma-divider-span" />
+          <RouterLink to="/login" class="font-bold"> Login </RouterLink>
+          <hr class="ma-divider-span" />
+          <a
+            href="https://searchads.com/?__hstc=212712200.c711629e081c1750ce8d4392ba12b1c6.1683644286291.1689761584422.1690370044196.8&amp;__hssc=212712200.1.1690370044196&amp;__hsfp=147110142"
+            target="_blank"
+            rel="noopener"
+            class="pr-1"
+          >
+            Go to <b>SearchAds</b></a
+          >
+        </div>
       </div>
     </div>
     <div class="ma-header">
-      <RouterLink class="ma-logo" to="/">
-        <MaIcons name="MobileActionLogo" />
-      </RouterLink>
-      <div class="ma-buttons-together">
+      <div class="ma-header-logo-wrapper">
+        <RouterLink class="ma-router-logo" to="/">
+          <MaIcons name="MobileActionLogo" />
+        </RouterLink>
+      </div>
+      <div class="ma-buttons-wrapper">
         <MaButton intent="header-primary" class="">Schedule a demo</MaButton>
         <MaButton intent="header-primary" class="ma-button">
           <RouterLink to="/register"> Sign up free </RouterLink>
@@ -49,22 +53,34 @@ import MaButton from "../components/MaButton.vue";
 }
 
 .ma-infos {
-  @apply flex gap-5;
+  @apply flex;
 }
-
+.ma-infos-wrapper {
+  @apply max-w-[720px] min-w-[420px] gap-5 flex;
+}
 .ma-header {
-  @apply h-[70px] flex items-center fixed z-[100] shadow-[0_1px_5px_rgba(0,0,0,0.1)] w-screen left-0 top-[1.8rem] bg-white;
+  @apply h-[70px] justify-between flex items-center fixed z-[100] shadow-[0_1px_5px_rgba(0,0,0,0.1)] w-screen left-0 top-[1.8rem] bg-white;
 }
 
-.ma-logo {
-  @apply h-[22px];
+.ma-header-logo-wrapper {
+  @apply flex ml-20;
+}
+
+.ma-router-logo {
+  @apply flex justify-center align-middle content-center flex-wrap;
 }
 
 .ma-button {
   @apply bg-[#de2442] text-white border-[#de2442];
 }
 
-.ma-buttons-together {
-  @apply flex w-full justify-end gap-4;
+.ma-button:hover {
+  @apply text-white bg-[#f70202] border-[#de2342];
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.ma-buttons-wrapper {
+  @apply flex min-w-[460px] gap-4;
 }
 </style>
